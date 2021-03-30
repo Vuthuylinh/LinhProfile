c0edb4dc51ff761cee626aab0a6ee113c66a994a
Linh Vu <thuylinh2490@gmail.com>
2021-03-30 17:46:30 -0400

    initialize project with webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+//https://webpack.js.org/configuration/dev-server/
+
+const path = require('path');
+module.exports={
+  entry:'./client/index.js',
+  mode:'development',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  devtool: 'source-map',
+  devServer: {
+		contentBase: path.join(__dirname, 'public'),
+		port: 8000,
+	},
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  }
+}
