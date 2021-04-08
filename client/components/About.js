ada70d74a1a4c5219fd7a8362f52678e442373da
Linh Vu <thuylinh2490@gmail.com>
2021-04-08 18:52:28 -0400

    fix(About): testing

--- client/components/About.js
+++ client/components/About.js
@@ -2,6 +2,10 @@ import React from 'react'
 import logo from '../media/logo2.png'
 export default function About(){
   return(
+    <div>
+      <h1>Welcome to Linh Vu website !</h1>
     <img src={logo}/>
+    </div>
+
   )
 }
