f523f83afdd0fb9a18b5fe4feb9afe23483c1561
Linh Vu <thuylinh2490@gmail.com>
2021-05-08 22:09:42 -0400

    adjust css

--- client/components/About.js
+++ client/components/About.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import logo from '../media/logo2.png'
-export default function About(){
-  return(
+export default function About() {
+  return (
     <div>
-      <h1>Welcome to Linh Vu website !</h1>
-    <img src={logo}/>
+      <img src={logo}/>
+      <h1> Welcome to Linh Vu website !</h1>
+
     </div>
 
   )
