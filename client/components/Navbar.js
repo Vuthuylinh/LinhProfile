65e2622c4f0382e25544c788eeb829f26ce0ef1a
Linh Vu <thuylinh2490@gmail.com>
2021-05-16 23:11:15 -0500

    feat(Navbar): test connect Navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,1 +1,13 @@
 import React from 'react'
+import logo from '../media/logo2.png'
+export default function Navbar() {
+  return (
+      <div>
+          <h1>Home</h1>
+ <img src={logo}/>
+
+      </div>
+  
+  )
+}
+
