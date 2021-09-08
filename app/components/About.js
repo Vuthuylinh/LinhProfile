386f8d4d2db843041c87e326903739aa9302b59d
Linh Vu <thuylinh2490@gmail.com>
2021-09-07 22:46:42 -0400

    adjust svg text position

--- app/components/About.js
+++ app/components/About.js
@@ -1,40 +1,40 @@
 import React from 'react'
 import linhPhoto from '../media/profilePictureLinh.png'
-// import {Helmet} from "react-helmet";
 export default function About() {
   return (
-      <section class="about-me">
-        <div class="intro">
-          <article class="typewriter">
-            <h1>Hi there! I'm Linh Vu</h1>
-          </article>
-          <h1>
-            <a
-              href=""
-              class="typewrite"
-              data-period="2000"
-              data-type='[ "I am a plants mom", "I was a banker", "Love to travel", "I Love to Develop." ]'
-            >
-              <span class="wrap"></span>
-            </a>
-          </h1>
+    <section class="about-me">
+      <div class="intro">
+        <article class="typewriter">
+          <h1>Hi there! I'm Linh Vu</h1>
+        </article>
+        <h1>
+          <a
+            href=""
+            class="typewrite"
+            data-period="2000"
+            data-type='[ "I am a plants mom", "I was a banker", "Love to travel", "I Love to Develop." ]'
+          >
+            <span class="wrap"></span>
+          </a>
+        </h1>
+      </div>
+      <div class="profile-wrapper">
+        <div class="profile">
+          <img
+            id="profile-image"
+            src={linhPhoto}
+            alt="Linh's picture"
+          />
         </div>
-        <div class="profile-wrapper">
-          <div class="profile">
-            <img
-              id="profile-image"
-              src={linhPhoto}
-              alt="Linh's picture"
-            />
-          </div>
-          <div class="svg-wrapper">
-            <svg height="60" width="320" xmlns="http://www.w3.org/2000/svg">
-              <rect class="shape" height="50" width="200" />
-              <div class="text">LINH VU</div>
-            </svg>
-          </div>
+        <div class="svg-wrapper">
+          <svg height="400" width="400" xmlns="http://www.w3.org/2000/svg">
+              <rect x="0" y="350" class="shape" height="50" width="200" />
+              <text x="30" y="385" fill="#e02932" class="text" >LINH VU</text>
+          </svg>
         </div>
-      </section>
+      </div>
+
+    </section>
 
   )
 }
