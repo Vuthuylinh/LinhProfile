cbb44b64b46fee7e2b62a57028bd658443aece1c
Linh Vu <thuylinh2490@gmail.com>
2021-09-09 23:15:43 -0400

    add techstack

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -33,6 +33,7 @@ export default function Contact() {
       >
         <img src="https://img.icons8.com/dusk/50/000000/email.png" />
       </a>
+      <a href="https://icons8.com/photos">Photo by Icons8</a>
     </div>
   )
 }
---
+++ app/components/TechStack.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+export default function TechStack(){
+  return(
+    <div>
+    <section className="proficient">
+      <ul>
+        <li>JavaScript</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>React</li>
+        <li>Redux</li>
+        <li>Angular</li>
+        <li>HTML</li>
+        <li>Flexbox</li>
+        <li>CSS</li>
+        <li>PostgreSQL</li>
+        <li>PouchDB</li>
+        <li>CouchDB</li>
+        <li>Git and Github </li>
+      </ul>
+    </section>
+    <section className="familiar">
+      <ul>
+        <li>SQL</li>
+        <li>jQuery</li>
+        <li>Firebase</li>
+        <li>React Native</li>
+        <li>Webpack</li>
+        <li>Webscraping</li>
+        <li>Chai</li>
+        <li>Mocha</li>
+        <li>Jasmine</li>
+        <li>Heroku</li>
+        <li>CI/CD with Travis CI</li>
+        <li>Google Extensions</li>
+        <li>TDD </li>
+      </ul>
+    </section>
+    <section className="colaboration tools">
+      <ul>
+        <li>Jira</li>
+        <li>Slack</li>
+        <li>GitHub Projects</li>
+        <li>Trello</li>
+        <li>Microsoft Teams</li>
+      </ul>
+    </section>
+    </div>
+  )
+}
