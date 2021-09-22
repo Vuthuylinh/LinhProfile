90d60d87e1c95f9a909159dac98a3868f88de1c7
Linh Vu <thuylinh2490@gmail.com>
2021-09-21 23:36:42 -0400

    change cover

--- app/components/About.js
+++ app/components/About.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import linhPhoto from '../media/profilePictureLinh.png'
+import cover from '../media/cover.png'
 export default function About() {
   return (
     <section id="about-me">
+      <img
+        id="profile-image"
+        src={cover}
+        alt="Linh's picture"
+      />
       <div class="intro">
-        <article class="typewriter">
+      <article class="typewriter">
           <h1>Hi there! I'm Linh Vu</h1>
         </article>
         <h1>
@@ -18,21 +23,7 @@ export default function About() {
           </a>
         </h1>
       </div>
-      <div class="profile-wrapper">
-        <div class="profile">
-          <img
-            id="profile-image"
-            src={linhPhoto}
-            alt="Linh's picture"
-          />
-        </div>
-        {/* <div className="svg-wrapper">
-          <svg height="400" width="400" xmlns="http://www.w3.org/2000/svg">
-            <rect x="0" y="350" className="shape" height="50" width="200" />
-            <text x="30" y="385" fill="#e02932" className="text" >LINH VU</text>
-          </svg>
-        </div> */}
-      </div>
+
 
     </section>
 
